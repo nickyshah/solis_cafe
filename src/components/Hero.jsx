dcd7f75ad16dba2b1b7e546ad2c00d957594d9b0
Nick Shah <nick@Nicks-MacBook-Air.local>
2025-05-17 12:06:56 +1000

    nav bar fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Hero() {
   return (
-    <header id="home" className="bg-dark text-white text-center py-5">
+    <header id="home" className="bg-dark text-white text-center py-5 mt-5">
       <div className="container">
         <h1>Welcome to Café Aroma</h1>
-        <p className="lead">Where every sip is a story</p>
+        <p className="lead">Where every coffee sip is a story</p>
       </div>
     </header>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 export default function Navbar() {
+const[isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <div className="container">
         <a className="navbar-brand" href="#">Solis Espresso</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <button 
+        onClick={toggleMenu}
+        className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`collapse navbar-collapse ${isOpen ? 'show d-flex p-5' : ''}`} id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item"><a className="nav-link" href="#home">Home</a></li>
             <li className="nav-item"><a className="nav-link" href="#menu">Menu</a></li>
